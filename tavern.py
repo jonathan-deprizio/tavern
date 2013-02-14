9a61fb6c37b02d998e929d2a8122bf7d76c3947c
Jonathan DePrizio <jondeprizio@gmail.com>
2013-02-13 20:56:00 -0500

    It didn't work before.  Now it does.

--- tavern.py
+++ tavern.py
@@ -21,7 +21,7 @@ WIRELESS_PROVIDER = "verizonwireless"
 #  End of user-configurable options
 ####################
 
-WIRELESS_LOOKUP = {'verizonwireless':'vtext.com'}
+WIRELESS_LOOKUP = {'verizonwireless':'vtext.com',}
 
 import subprocess, smtplib, sys
 from email.mime.text import MIMEText
@@ -31,12 +31,17 @@ from socket import gethostname
 
 options = OptionParser()
 options.add_option("-n", help="Your cell number.", default=False, dest="cellNumber")
-options.add_option("-c", help="The command you want to run.", dest="cmd", default=False)
+options.add_option("-e", help="Your email address.", default=False, dest="emailAddress")
+options.add_option("-c", help="The command you want to run.", dest="cmd", default=None)
 opts, args = options.parse_args()
 
-if (not opts.cellNumber) or (not opts.cmd):
-        print "All arguments are required. Use -h for help."
+if not opts.cmd:
+	print "You must specify a command to run with -c."
         sys.exit(1)
+
+if (not opts.emailAddress) and (not opts.cellNumber):
+	print "You must specify either an email address (-e) or a cell number (-n)."
+	sys.exit(1)
 
 if WIRELESS_PROVIDER not in WIRELESS_LOOKUP.keys():
         print "Your wireless provider is not supported."
@@ -46,6 +51,8 @@ DEST_EMAIL = str(opts.cellNumber) + "@" + WIRELESS_LOOKUP[WIRELESS_PROVIDER]
 
 cmdExec = subprocess.Popen([opts.cmd], shell=True)
 rt = cmdExec.wait()
+
+msgBody = opts.cmd + " rt=" + str(rt)
 msg = MIMEMultipart('alternative')
 msg['From'] = gethostname()
 msg['To'] = DEST_EMAIL
