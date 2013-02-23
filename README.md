e83492b27314ca7d3e697da1340c9348bca91cc9
Jonathan DePrizio <jondeprizio@gmail.com>
2013-02-23 01:26:37 -0500

    Switch from CLI to config based setup.  Other changes.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ tavern
 ======
 
 Text Alert Vehicle for Exit Returncode Notification
+
+
+Limitations
+======
+
+Only one phone number and one email address are supported.  You may be able to get away with comma-seperating email addresses,
+but definitely not phone numbers (not tested).
+
--- tavern.py
+++ tavern.py
@@ -5,65 +5,112 @@
 # Released into the public domain
 
 ####################
-#  Configuration - this is required.
-
-SMTPSERVER=""
-SMTPUSER=""
-SMTPPASS=""
-SMTPPORT=5125
-
-# Your wireless provider.  Currently supported: verizonwireless.
-# It's easy to add more; just add them to the dictionary above
-# the import lines below.
-
-WIRELESS_PROVIDER = "verizonwireless"
-
-#  End of user-configurable options
 ####################
 
-WIRELESS_LOOKUP = {'verizonwireless':'vtext.com','att':'txt.att.net'}
+WIRELESS_LOOKUP = {	'VerizonWireless':'vtext.com',
+			'ATT':'txt.att.net',
+			'SprintPCS-CDMA':'messaging.sprintpcs.com',
+			'VirginMobile':"vmobi.com"}
+
+DEFAULT_CONFIG = "~/.tavernrc"
 
 import subprocess, smtplib, sys
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from optparse import OptionParser
 from socket import gethostname
+import os
+
+CONFIGITEMS={	'SMTPSERVER':'',
+		'SMTPUSER':'',
+		'SMTPPASS':'',
+		'SMTPSSL':'',
+		'SMTPPORT':'',
+
+		'PHONETARGET':'undef',
+		'EMAILTARGET':'undef',
+		'WIRELESSPROVIDER':'undef'}
+
+def parseConfigFile(configLines):
+	# goes through the lines read from the file specified with -f 
+	# and fills in proper details.
+	#
+	# Required entries: SMTPSERVER, SMTPUSER, SMTPPASS, SMTPSSL, SMTPPORT
+	# Optional entries: PHONETARGET, EMAILTARGET, WIRELESSPROVIDER
+
+	for rawline in configLines:
+		line = rawline.split("#")[0].replace("\n", "")
+		
+		if (line.count("=") != 1):
+			if len(line) != 0:
+				print "Warning - invalid line in config: " + line
+				continue
+		else: 
+			line = line.split("=") 
+			descriptor = line[0]
+			value = line[1]
+			if descriptor in CONFIGITEMS:
+				CONFIGITEMS[descriptor] = value
+			else:
+				print "Warning - Unrecognized configuration option: " + descriptor
+
+	print CONFIGITEMS
+	print "Done parsing config."
 
 options = OptionParser()
-options.add_option("-n", help="Your cell number.", default=False, dest="cellNumber")
-options.add_option("-e", help="Your email address.", default=False, dest="emailAddress")
 options.add_option("-c", help="The command you want to run.", dest="cmd", default=None)
+options.add_option("-f", help="Configuration file.  Default is ~/.tavernrc", dest="configFile", default=DEFAULT_CONFIG)
+options.add_option("-v", help="Show debug messages when connecting to the mail server.", dest="mailDebug", default=False,action="store_true")
 opts, args = options.parse_args()
+
+opts.configFile = os.path.expanduser(opts.configFile)
+try:
+	cf = open(opts.configFile, 'r')
+except:
+	if opts.configFile == DEFAULT_CONFIG:
+		# user didn't specify a config file
+		print "Please setup your configuration file in ~/.tavernrc"
+		print "An example configuration file is provided for your enjoyment."
+		sys.exit(1)
+	else:
+		print "Failed to open specified config file: " + opts.configFile
+		sys.exit(1)
+else:
+	parseConfigFile(cf.readlines())
+	cf.close()
 
 if not opts.cmd:
 	print "You must specify a command to run with -c."
         sys.exit(1)
 
-if (not opts.emailAddress) and (not opts.cellNumber):
-	print "You must specify either an email address (-e) or a cell number (-n)."
-	sys.exit(1)
+if (CONFIGITEMS["WIRELESSPROVIDER"] != "undef"):
+	if (CONFIGITEMS["WIRELESSPROVIDER"] not in WIRELESS_LOOKUP.keys()):
+		print "Your wireless provider is not supported."
+		sys.exit(1)
 
-if WIRELESS_PROVIDER not in WIRELESS_LOOKUP.keys():
-        print "Your wireless provider is not supported."
-        sys.exit(1)
+DEST_EMAIL_ADDRESSES = []
+if CONFIGITEMS["PHONETARGET"] != "undef":
+	DEST_EMAIL_ADDRESSES.append(str(CONFIGITEMS["PHONETARGET"]) + "@" + WIRELESS_LOOKUP[CONFIGITEMS["WIRELESSPROVIDER"]])
+if CONFIGITEMS["EMAILTARGET"] != "undef":
+	DEST_EMAIL_ADDRESSES.append(CONFIGITEMS["EMAILTARGET"])
 
-DEST_EMAIL = str(opts.cellNumber) + "@" + WIRELESS_LOOKUP[WIRELESS_PROVIDER]
-
+print "EMailiing: " + str(DEST_EMAIL_ADDRESSES)
 cmdExec = subprocess.Popen([opts.cmd], shell=True)
 rt = cmdExec.wait()
 
 msgBody = opts.cmd + " rt=" + str(rt)
 msg = MIMEMultipart('alternative')
 msg['From'] = gethostname()
-msg['To'] = DEST_EMAIL
 msg.attach(MIMEText(msgBody, 'plain'))
 
-try:
-        mailconn = smtplib.SMTP(SMTPSERVER, SMTPPORT)
-        mailconn.set_debuglevel(1)
-        mailconn.login(SMTPUSER, SMTPPASS)
-        mailconn.sendmail(SMTPUSER, DEST_EMAIL, msg.as_string())
-        mailconn.quit()
-except:
-        print "An error occurred sending the email.  Uh oh!"
-        sys.exit(1)
+for DEST_EMAIL in DEST_EMAIL_ADDRESSES:
+	msg['To'] = DEST_EMAIL
+	try:
+		mailconn = smtplib.SMTP_SSL(CONFIGITEMS["SMTPSERVER"], CONFIGITEMS["SMTPPORT"])
+		mailconn.set_debuglevel(opts.mailDebug)
+		mailconn.login(CONFIGITEMS["SMTPUSER"], CONFIGITEMS["SMTPPASS"])
+		mailconn.sendmail(CONFIGITEMS["SMTPUSER"], DEST_EMAIL, msg.as_string())
+		mailconn.quit()
+	except:
+		print "An error occurred sending the email to " + DEST_EMAIL + ".  Uh oh!"
+		sys.exit(1)
